c2871a8d4e1713a1406e9f17ed2167cc1784b3fe
Kevin Eddie <keddie024@gmail.com>
2021-05-16 14:36:05 -0400

    moved contents of develop folder. added model for workout

---
+++ models/WorkoutSchema.js
@@ -0,0 +1,41 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const WorkoutSchema = new Schema({
+    day: {
+        type: Date,
+        default: Date.now,
+        unique: true
+    },
+    exercises: [
+        {
+            type: {
+                type: String,
+                unique: false
+            },
+            name: {
+                type: String
+            },
+            distance: {
+                type: Number
+            },
+            duration: {
+                type: Number
+            },
+            weight: {
+                type: Number
+            },
+            sets: {
+                type: Number
+            },
+            reps: {
+                type: Number
+            }
+        },
+    ]
+});
+
+const Workout = mongoose.model("Workout", WorkoutSchema);
+
+module.exports = Workout;
